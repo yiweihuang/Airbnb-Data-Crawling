ee556c55eb91091d41f215ee6300dc30eca6ac91
yiweihuang <n1993212@gmail.com>
2016-11-07 14:24:07 +0800

    Modify the structure

--- cityname/spiders/airbnb_site.py
+++ cityname/spiders/airbnb_site.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 import scrapy
-from scrapy.selector import Selector
-from scrapy.http import HtmlResponse
 from cityname.items import CitynameItem
 
 class AirbnbSiteSpider(scrapy.Spider):
@@ -11,7 +9,14 @@ class AirbnbSiteSpider(scrapy.Spider):
 
     def parse(self, response):
         item = CitynameItem()
-        full_name_city = response.xpath('//div[@class="contentContainer"]/h2/text()')
-        for city_name in full_name_city:
-            item['full_name_city'] = city_name.extract()
-            yield item
+        cities_detail = response.xpath('//div[@class="contentContainer"]')
+
+        for city in cities_detail:
+            full_name_city = city.xpath('h2/text()').extract()
+            table_info = city.xpath('table/tbody/tr')
+            for name in full_name_city:
+                item['full_name_city'] = name
+                yield item
+            # for i in table_info:
+            #     print(i.xpath('td/text()').extract())
+            #     print(i.xpath('td/a/text()').extract())
